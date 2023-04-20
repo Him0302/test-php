42af95fba11850003c14b121135a6995deff2107
Him0302 <81606642+Him0302@users.noreply.github.com>
2023-04-20 18:01:44 +0530

    Update config.php

--- web/config.php
+++ web/config.php
@@ -5,7 +5,7 @@ session_start();
 $db_host = "database-1.ci3hffp1cf30.us-east-1.rds.amazonaws.com";
 $db_user = "admin";
 $db_pass = "Him8843*";
-$db_name = "";
+$db_name = "example";
 
 $mysql = new mysqli($db_host, $db_user, $db_pass, $db_name);
 $mysql->set_charset("utf8");
