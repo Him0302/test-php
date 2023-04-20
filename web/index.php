225765eae9e731e9ed4262816bb4b2effcf07bad
Him0302 <81606642+Him0302@users.noreply.github.com>
2023-04-20 18:20:03 +0530

    Update index.php

--- web/index.php
+++ web/index.php
@@ -184,7 +184,7 @@
                                 </div>
                                 <div class="col-xs-9 text-right">
                                     <div class="huge"><?php echo $tipos->num_rows; ?></div>
-                                    <div>hi it's me</div>
+                                    <div>hi jii</div>
                                 </div>
                             </div>
                         </div>
